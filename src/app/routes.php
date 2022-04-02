4cec87b8f9fe2ff24d28de2403a9bc6752a83cea
AbdouShalby <abdoushalby@gmail.com>
2022-04-02 21:42:16 +0200

    Controller For Each Page

--- src/app/routes.php
+++ src/app/routes.php
@@ -2,12 +2,12 @@
 
 // Static pages routes
 $router->addRoute('', ['controller' => 'Index', 'action' => 'home']);
-$router->addRoute('about', ['controller' => 'Index', 'action' => 'about']);
-$router->addRoute('products', ['controller' => 'Index', 'action' => 'products']);
-$router->addRoute('contact', ['controller' => 'Index', 'action' => 'contact']);
-$router->addRoute('terms', ['controller' => 'Index', 'action' => 'terms']);
-$router->addRoute('login', ['controller' => 'Index', 'action' => 'login']);
-$router->addRoute('signup', ['controller' => 'Index', 'action' => 'signup']);
-$router->addRoute('forgot', ['controller' => 'Index', 'action' => 'forgot']);
+$router->addRoute('about', ['controller' => 'About', 'action' => 'about']);
+$router->addRoute('products', ['controller' => 'Product', 'action' => 'products']);
+$router->addRoute('contact', ['controller' => 'Contact', 'action' => 'contact']);
+$router->addRoute('terms', ['controller' => 'Terms', 'action' => 'terms']);
+$router->addRoute('login', ['controller' => 'Login', 'action' => 'login']);
+$router->addRoute('signup', ['controller' => 'Signup', 'action' => 'signup']);
+$router->addRoute('forgot', ['controller' => 'Forgot', 'action' => 'forgot']);
 
 $router->setParams(getUri());
--- src/controllers/Index.php
+++ src/controllers/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Controllers;
 
 class Index
@@ -6,40 +7,5 @@ class Index
   public function home()
   {
     view('front/home');
-  }
-
-  public function about()
-  {
-    view('front/about');
-  }
-
-  public function products()
-  {
-    view('front/products');
-  }
-
-  public function contact()
-  {
-    view('front/contact');
-  }
-
-  public function terms()
-  {
-    view('front/terms');
-  }
-
-  public function login()
-  {
-    view('front/login');
-  }
-
-	public function signup()
-  {
-    view('front/signup');
-  }
-
-	public function forgot()
-  {
-    view('front/forgot');
   }
 }
